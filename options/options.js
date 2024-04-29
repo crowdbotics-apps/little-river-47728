4203e3b8cb47d353212a3f653ca40d50ee160859
Crowdbotics <team@crowdbotics.com>
2024-04-29 01:45:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-river-47728",
+  url: "https://little-river-47728.botics.co",
+  api: "https://little-river-47728.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
